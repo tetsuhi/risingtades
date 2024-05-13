b095d99071b1549e7ad74c098e8a0958a9a0a4a3
tetsuhi <sntigg@gmail.com>
2024-05-13 13:33:02 +0200

    escena carta arreglada, mesa de juego creada

--- Assets/Scripts/cardBehaviour.gd
+++ Assets/Scripts/cardBehaviour.gd
@@ -1,9 +1,12 @@
-extends TextureRect
+class_name CartaUI
+extends Control
 
-const CARD_DELAY_SPEED = 10.0
+const CARD_DELAY_SPEED = 12.0
 
-@export var manoNodo: Node
 var isClicked: bool = false
+@onready var targets: Array[Node] = []
+@onready var carta_textura = $cartaTextura
+@onready var detector_colision = $detectorColision
 
 func _process(delta):
 	var mousePos: Vector2 = get_viewport().get_mouse_position()
@@ -15,10 +18,18 @@ func _process(delta):
 	if Input.is_action_just_released("click") and isClicked:
 		isClicked = false
 		
-	if not isClicked:
-		global_position = global_position.lerp(Vector2(manoNodo.position.x, manoNodo.position.y - size.y/2), delta * CARD_DELAY_SPEED)
+	#if not isClicked and manoNodo != null:
+	#	global_position = global_position.lerp(Vector2(manoNodo.position.x, manoNodo.position.y - size.y/2), delta * CARD_DELAY_SPEED)
 
 	if not isClicked and mousePos.x <= position.x + size.x and mousePos.y <= position.y + size.y and mousePos.x >= position.x and mousePos.y >= position.y:
 		scale = scale.lerp(Vector2(1.2, 1.2), delta*30)
 	else:
 		scale = scale.lerp(Vector2(1, 1), delta*30)
+
+
+func _on_detector_colision_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_detector_colision_area_exited(area):
+	pass # Replace with function body.
