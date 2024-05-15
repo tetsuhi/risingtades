7df74efdaedb356e52b8d070f08c43cfce86f400
tetsuhi <sntigg@gmail.com>
2024-05-15 12:28:09 +0200

    menús

---
+++ Assets/Scripts/boton_coleccion.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://Assets/Scenes/menu_coleccion.tscn")
--- Assets/Scripts/cardBehaviour.gd
+++ Assets/Scripts/cardBehaviour.gd
@@ -1,22 +1,27 @@
-class_name CartaUI
 extends Control
 
 const CARD_DELAY_SPEED = 12.0
 
 var isClicked: bool = false
-@onready var targets: Array[Node] = []
-@onready var carta_textura = $cartaTextura
-@onready var detector_colision = $detectorColision
+var lastPosition:= Vector2(0.0, 0.0)
+
+func _ready():
+	print(position)
 
 func _process(delta):
 	var mousePos: Vector2 = get_viewport().get_mouse_position()
-	if Input.is_action_pressed("click"):
+	if Input.is_action_pressed("LMB"):
+		lastPosition = position
 		if isClicked or mousePos.x <= position.x + size.x and mousePos.y <= position.y + size.y and mousePos.x >= position.x and mousePos.y >= position.y:
 			isClicked = true
 			position = position.lerp(mousePos - size/2, delta * CARD_DELAY_SPEED)
 	
-	if Input.is_action_just_released("click") and isClicked:
+	if Input.is_action_just_released("LMB") and isClicked:
 		isClicked = false
+	
+	#if isClicked and Input.is_action_pressed("RMB"):
+		#position = lastPosition
+		#isClicked = false 
 		
 	#if not isClicked and manoNodo != null:
 	#	global_position = global_position.lerp(Vector2(manoNodo.position.x, manoNodo.position.y - size.y/2), delta * CARD_DELAY_SPEED)
@@ -25,11 +30,3 @@ func _process(delta):
 		scale = scale.lerp(Vector2(1.2, 1.2), delta*30)
 	else:
 		scale = scale.lerp(Vector2(1, 1), delta*30)
-
-
-func _on_detector_colision_area_entered(area):
-	pass # Replace with function body.
-
-
-func _on_detector_colision_area_exited(area):
-	pass # Replace with function body.
