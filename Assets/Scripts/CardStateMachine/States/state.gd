6a13c8d19179657d723e01920ea88027e0369502
miguelmm95 <miguelmartinezmartinez95@gmail.com>
2024-05-19 19:41:26 +0200

    CardStateMachine base

---
+++ Assets/Scripts/CardStateMachine/States/state.gd
@@ -0,0 +1,18 @@
+extends Node
+
+class_name State
+
+var card : Control
+var next_state : State
+
+func state_process(delta):
+	pass
+	
+func state_input(event : InputEvent):
+	pass
+	
+func on_enter():
+	pass
+	
+func on_exit():
+	pass
